8c0da6f37cf8a57a433ee5888fbdd7c67e270d7e
Haiyun Wang <why7121@gmail.com>
2022-07-24 13:20:30 +1000

    add gameRecord.db and fix the state not update bug

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const prompts = require("prompts");
+const fs=require('fs');
+if (!fs.existsSync('gameRecord.db')){
+	fs.writeFileSync('gameRecord.db', '');
+}
 
-let state=0
+var sqlite3 = require('sqlite3').verbose();
+let db=new sqlite3.Database('./gameRecord.db',(err)=>{
+	if (err) {
+		console.error(red(err.message))
+	}
+});
 
 async function main(){
   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, win INTEGER NOT NULL, lose INTEGER NOT NULL, winrate REAL NOT NULL)');
   while(true){
-    if(state==0){
-      await displayMenu()
-    }else if(state==1){
+    let state = await displayMenu()
+    console.log('state',state)
+    if(state==1){
       await addNewPlayer()
     }else if(state==2){
       await recordMatch()
@@ -25,21 +34,24 @@ async function main(){
     }
   }
 }
-main()
+
+main();
+
 
 async function displayMenu(){
+  console.log('displayMenu')
   const response  = await prompts({
     type:'select',
-    name:'menus',
-    actions:[
+    name:'value',
+    message:'Choose your action',
+    choices:[
       {title:'Add new player',value:1},
       {title:'Record a match',value:2},
       {title:'Check leaderboard',value:3},
       {title:'Exit',value:4}
     ]
   })
-  state = response.value;
-  console.log('state',state)
+  return response.value;
 }
 
 async function addNewPlayer(){
@@ -62,11 +74,147 @@ async function addNewPlayer(){
 }
 
 async function recordMatch(){
+  return new Promise((resolve,reject)=>{
+    const sql='SELECT * FROM users ORDER BY id ASC'
+    db.all(sql,async (err,rows)=>{
+			if (err){
+				console.error(err.message)
+				resolve(false)
+				return
+			}
+
+			if (rows.length<=1){
+				let response = await prompts({
+					type: 'confirm',
+					name: 'value',
+					message: 'Must have two or more players. Do you wanna add a new player?',
+					initial: true
+				})
+
+				resolve(false)
+				return
+					
+			}
+
+			const response = await prompts({
+				type: 'select',
+				name: 'value',
+				message: "Who's the winner?",
+				choices: rows.map((row)=>{
+					return {
+						title: row.name,
+						value: row.id
+					}
+				}),
+				initial: 0
+			})
+
+			if (!response.value){
+				resolve(false)
+				console.error('Unexpected Input')
+				return
+			}
+
+			let winner=rows.filter(row=>row.id==response.value)[0]
+
+			const response2 = await prompts({
+				type: 'select',
+				name: 'value',
+				message: "Who's the loser?",
+				choices: rows.map((row)=>{
+					return {
+						title: row.name,
+						value: row.id
+					}
+				}),
+				initial: 0
+			})
+
+			if (!response2.value){
+				resolve(false)
+				console.error('Unexpected Input')
+				return
+			}
+
+			let loser=rows.filter(row=>row.id==response2.value)[0]
+
+			if (winner.id==loser.id){
+				console.error('You can not play against yourself')
+				resolve(false)
+				return
+			}
+
+			const confirmRecordResponse = await prompts({
+				type: 'toggle',
+				name: 'value',
+				message: `Are you sure you want to record ${winner.name} as winner and ${loser.name} as loser?`,
+				initial: true,
+				active: 'yes',
+				inactive: 'no'
+			})
+
+			if (!confirmRecordResponse.value){
+				console.log('Aborted')
+				resolve(false)
+				return
+			}
+			
+			winner.win+=1
+			loser.lose+=1
+
+			winner.winrate=((winner.win/(winner.win+winner.lose))*100).toFixed(1)
+			loser.winrate=((loser.win/(loser.win+loser.lose))*100).toFixed(1)
+
+			const sql2='UPDATE users SET win=?, lose=?, winrate=? WHERE id=?'
+			db.run(sql2,[winner.win,winner.lose,winner.winrate,winner.id])
+			db.run(sql2,[loser.win,loser.lose,loser.winrate,loser.id])
+
+			resolve(true)
+			
+		})
+  })
   
 }
 
 async function checkLeaderBoard(){
+  const sql='SELECT * FROM users ORDER BY winrate DESC, win DESC, lose DESC'
+  return new Promise((resolve,reject)=>{
+		db.all(sql,async (err,rows)=>{
+			if (err){
+				console.error(err.message)
+				resolve(false)
+				return
+			}
 
+			if (rows.length<=0){
+				let response = await prompts({
+					type: 'confirm',
+					name: 'value',
+					message: 'Database is empty.! Do you wanna add a new player?',
+					initial: true
+				})
+				resolve(false)
+        if(response.value){
+          state=1
+        }
+				return		
+			}
+
+			console.table(rows)
+	
+			const response = await prompts({
+				type: 'confirm',
+				name: 'value',
+				message: 'Do you want to go back?',
+				initial: true
+			})
+      if(response.value){
+        state = 0
+      }
+			resolve(true)
+		})
+
+	})
 }
 
 
